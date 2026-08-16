0641f166785ef284955c53424492f56132ba6f70
MohsinPAK-VC <mohsinhun23@gmail.com>
2026-08-16 18:39:22 +0500

    Added the theme js for all and connect theme and also sizing for other devices

--- js/theme.js
+++ js/theme.js
@@ -8,5 +8,40 @@ function applyGlobalTheme() {
     const theme =
       localStorage.getItem(themekey) || "day";
 
-  document.body.class    
+  document.body.classList.toggle(
+    "night-mode",
+    theme === "night"
+  );
+  
+  const button = 
+     document.getElementsBy("themeButton");
+
+     if (button) {
+     button.textContext =
+      theme === "night"
+             ? "☀️ Day Mode"
+             : "🌛 Night Mode";
+     }
 }
+
+function toggleGlobalTheme() {
+    const isNight =
+      document.body.classList.contains("night-mode");
+   const newTheme =
+         isNight ? "day" : "night";
+    localStorage.setItem(
+        themekey,
+        newTheme
+    );        
+
+    applyGlobalTheme();
+
+}
+applyGlobalTheme();
+
+document.addEventListener("click", (event) => {
+    const button =
+       event.target.closest("#themeButton");
+    if (!button) return;
+    toggleGlobalTheme();   
+})
